ff1036d8586cbd62045f564e03708fe7f9727795
Ajay Kiran Reddy <ajaykiranreddy@IN1-1026681MBP.local>
2020-09-22 14:37:32 +0530

    changed repo

--- Server/client/src/utils/sportsUtils.js
+++
@@ -1,8 +0,0 @@
-export  function getMatchStatusDetails (data,status){
-    if(status==='LIVE'){
-        return data.matches.filter(match=>((match.status === status) || (match.status ==="INPROGRESS")))
-    }
-    else{
-        return data.matches.filter(match=>match.status === status);
-    }
-}
